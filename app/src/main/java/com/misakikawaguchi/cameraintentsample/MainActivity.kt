6ae1ccbad20b81cbdd927280493d48d7ad4c5707
川口美咲 <misaki@arcphilia.co.jp>
2021-03-01 18:45:54 +0900

    [Add]画面がタップされたときの処理を追加

--- app/src/main/java/com/misakikawaguchi/cameraintentsample/MainActivity.kt
+++ app/src/main/java/com/misakikawaguchi/cameraintentsample/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.misakikawaguchi.cameraintentsample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    // 画面がタップされた時の処理
+    fun onCameraImageClick(view: View) {
+        // Intentオブジェクトを生成
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        // アクティビティを起動
+        startActivityForResult(intent, 200)
     }
 }
