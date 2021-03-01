d0f811bc7ad0d72ff8217a38baf410610ecd929e
川口美咲 <misaki@arcphilia.co.jp>
2021-03-01 20:29:54 +0900

    [Modify]カメラアプリ起動処理を変更

--- app/src/main/java/com/misakikawaguchi/cameraintentsample/MainActivity.kt
+++ app/src/main/java/com/misakikawaguchi/cameraintentsample/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.misakikawaguchi.cameraintentsample
 
-import android.app.Activity
+import android.Manifest
+import android.content.ContentValues
 import android.content.Intent
-import android.graphics.Bitmap
+import android.content.pm.PackageManager
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
 import android.widget.ImageView
+import androidx.core.app.ActivityCompat
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -35,6 +39,33 @@ class MainActivity : AppCompatActivity() {
 
     // 画面がタップされた時の処理
     fun onCameraImageClick(view: View) {
+        // WRITE_EXTERNAL_STORAGEの許可が降りていない場合
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            // WRITE_EXTERNAL_STORAGEの許可を求めるダイアログを表示。その際、リクエストコードを2000に設定
+            val permissions = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            ActivityCompat.requestPermissions(this, permissions, 200)
+            return
+        }
+
+        // 日時データを「yyyyMMddHHmmss」の形式に整形するフォーマッタを生成
+        val dateFormat = SimpleDateFormat("yyyyMMddHHmmss")
+        // 現在の日時を取得
+        val now = Date()
+        // 取得した日時データを「yyyyMMddHHmmss」形式に整形した文字列を生成
+        val nowStr = dateFormat.format(now)
+        // ストレージに格納する画像のファイル名を生成。ファイルの一意性を確保するためにタイムスタンプの値を利用
+        val fileName = "UseCameraActivityPhoto_${nowStr}.jpg"
+
+        // ContentValuesオブジェクトを生成
+        val values = ContentValues()
+        /// 画像ファイル名を設定
+        values.put(MediaStore.Images.Media.TITLE, fileName)
+        // 画像ファイルの種類を設定
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+
+        //ContentResolverを使ってURIオブジェクトを生成
+        _imageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+
         // Intentオブジェクトを生成
         val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         // アクティビティを起動
