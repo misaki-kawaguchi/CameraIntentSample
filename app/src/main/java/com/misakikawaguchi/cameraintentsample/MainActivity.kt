007b9df53ecc8946deb132a46f2da85daa79f44d
川口美咲 <misaki@arcphilia.co.jp>
2021-03-01 19:45:10 +0900

    [Add]処理が元のアクティビティに戻ってきたときに実行されるメソッドを追加

--- app/src/main/java/com/misakikawaguchi/cameraintentsample/MainActivity.kt
+++ app/src/main/java/com/misakikawaguchi/cameraintentsample/MainActivity.kt
@@ -1,15 +1,33 @@
 package com.misakikawaguchi.cameraintentsample
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    // 処理が元のアクティビティに戻ってきたときに実行されるメソッド
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // カメラアプリからの戻りでかつ撮影成功の場合
+        if(requestCode == 200 && resultCode == RESULT_OK) {
+            // 撮影されたビットマップデータを取得
+            val bitmap = data?.getParcelableExtra<Bitmap>("data")
+            // 画像を表示するImageViewを取得
+            val ivCamera = findViewById<ImageView>(R.id.ivCamera)
+            // 撮影された画像をImageVIewに設定
+            ivCamera.setImageBitmap(bitmap)
+        }
     }
 
     // 画面がタップされた時の処理
